0cf80cc75eec8d7d85c612ae28098464a14034dd
Jenda Mudron <jenmud@gmail.com>
2020-04-19 09:18:39 +0000

    added in the RPC method for node operations

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -7,24 +7,76 @@ import (
 	pb "github.com/jenmud/draft/service"
 )
 
+func convertGraphPropsToServiceProps(props map[string]graph.Value) map[string]*pb.Value {
+	p := make(map[string]*pb.Value)
+
+	for k, v := range props {
+		p[k] = &pb.Value{Type: v.Type, Value: v.Value}
+	}
+
+	return p
+}
+
 type server struct {
 	graph *graph.Graph
 }
 
 func (s *server) AddNode(ctx context.Context, req *pb.NodeReq) (*pb.NodeResp, error) {
-	panic("NotImplemented")
+	kvs := make([]graph.KV, len(req.Properties))
+
+	count := 0
+	for k, v := range req.Properties {
+		kv := graph.KV{
+			Key: k,
+			Value: graph.Value{
+				Type:  v.Type,
+				Value: v.Value,
+			},
+		}
+
+		kvs[count] = kv
+		count++
+	}
+
+	node, err := s.graph.AddNode(req.Uid, req.Label, kvs...)
+	if err != nil {
+		return nil, err
+	}
+
+	resp := pb.NodeResp{
+		Uid:        node.UID,
+		Label:      node.Label,
+		Properties: convertGraphPropsToServiceProps(node.Properties),
+	}
+
+	return &resp, nil
 }
 
 func (s *server) RemoveNode(ctx context.Context, req *pb.UIDReq) (*pb.RemoveResp, error) {
-	panic("NotImplemented")
+	s.graph.RemoveNode(req.Uid)
+	return &pb.RemoveResp{Uid: req.Uid, Success: true}, nil
 }
 
 func (s *server) Node(ctx context.Context, req *pb.UIDReq) (*pb.NodeResp, error) {
-	panic("NotImplemented")
+	node, err := s.graph.GetNode(req.Uid)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.NodeResp{Uid: node.UID, Label: node.Label, Properties: convertGraphPropsToServiceProps(node.Properties)}, nil
 }
 
 func (s *server) Nodes(req *pb.NodesReq, stream pb.Graph_NodesServer) error {
-	panic("NotImplemented")
+	iter := s.graph.Nodes()
+	for iter.Next() {
+		node := iter.Value().(graph.Node)
+		resp := pb.NodeResp{Uid: node.UID, Label: node.Label, Properties: convertGraphPropsToServiceProps(node.Properties)}
+		if err := stream.Send(&resp); err != nil {
+			return nil
+		}
+	}
+
+	return nil
 }
 
 func (s *server) AddEdge(ctx context.Context, req *pb.EdgeReq) (*pb.EdgeResp, error) {
