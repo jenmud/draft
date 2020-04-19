11002769b9b5beff94074661eb8c4d9c823500b8
Jenda Mudron <jenmud@gmail.com>
2020-04-19 10:22:17 +0000

    completed the edge code on the graph

--- graph/graph_edge.go
+++ graph/graph_edge.go
@@ -1,6 +1,19 @@
 package graph
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/jenmud/draft/graph/iterator"
+)
+
+// HasEdge returns true if the graph has a edge with the provided uid.
+func (g *Graph) HasEdge(uid string) bool {
+	g.lock.RLock()
+	defer g.lock.RUnlock()
+
+	_, ok := g.edges[uid]
+	return ok
+}
 
 // AddEdge adds a new edge to the graph.
 func (g *Graph) AddEdge(uid, sourceUID, label, targetUID string, kv ...KV) (Edge, error) {
@@ -22,4 +35,39 @@ func (g *Graph) AddEdge(uid, sourceUID, label, targetUID string, kv ...KV) (Edge
 	edge := NewEdge(uid, sourceUID, label, targetUID, kv...)
 	g.edges[edge.UID] = edge
 	return edge, nil
+}
+
+// RemoveEdge removes the edge from the graph.
+func (g *Graph) RemoveEdge(uid string) {
+	g.lock.Lock()
+	defer g.lock.Unlock()
+	delete(g.edges, uid)
+}
+
+// Edge returns the edge with the provided uid.
+func (g *Graph) Edge(uid string) (Edge, error) {
+	g.lock.RLock()
+	defer g.lock.RUnlock()
+
+	edge, ok := g.edges[uid]
+	if !ok {
+		return Edge{}, fmt.Errorf("[GetEdge] No such edge with UID %s found", uid)
+	}
+
+	return edge, nil
+}
+
+// Edges returns a edge iterator.
+func (g *Graph) Edges() Iterator {
+	g.lock.RLock()
+	defer g.lock.RUnlock()
+
+	edges := make([]interface{}, len(g.edges))
+	count := 0
+	for _, edge := range g.edges {
+		edges[count] = edge
+		count++
+	}
+
+	return iterator.New(edges)
 }
--- graph/graph_edge_test.go
+++ graph/graph_edge_test.go
@@ -38,3 +38,89 @@ func TestAddEdge_missing_target(t *testing.T) {
 	assert.NotNil(t, err)
 	assert.Equal(t, Edge{}, actual)
 }
+
+func TestAddEdge_duplicate_uid(t *testing.T) {
+	g := New()
+
+	n1, _ := g.AddNode("node-1", "person")
+	n2, _ := g.AddNode("node-2", "person")
+	g.AddEdge("edge-1234", n1.UID, "knows", n2.UID, KV{Key: "since", Value: Value{Type: "string", Value: []byte("school")}})
+	actual, err := g.AddEdge("edge-1234", n1.UID, "knows", n2.UID, KV{Key: "since", Value: Value{Type: "string", Value: []byte("school")}})
+
+	assert.NotNil(t, err)
+	assert.Equal(t, Edge{}, actual)
+}
+
+func TestHasEdge(t *testing.T) {
+	g := New()
+
+	n1, _ := g.AddNode("node-1", "person")
+	n2, _ := g.AddNode("node-2", "person")
+	g.AddEdge("edge-1234", n1.UID, "knows", n2.UID, KV{Key: "since", Value: Value{Type: "string", Value: []byte("school")}})
+
+	assert.Equal(t, true, g.HasEdge("edge-1234"))
+}
+
+func TestHasEdge_missing(t *testing.T) {
+	g := New()
+
+	n1, _ := g.AddNode("node-1", "person")
+	n2, _ := g.AddNode("node-2", "person")
+	g.AddEdge("edge-1234", n1.UID, "knows", n2.UID, KV{Key: "since", Value: Value{Type: "string", Value: []byte("school")}})
+
+	assert.Equal(t, false, g.HasEdge("missing"))
+}
+
+func TestRemoveEdge(t *testing.T) {
+	g := New()
+
+	n1, _ := g.AddNode("node-1", "person")
+	n2, _ := g.AddNode("node-2", "person")
+	g.AddEdge("edge-1234", n1.UID, "knows", n2.UID, KV{Key: "since", Value: Value{Type: "string", Value: []byte("school")}})
+
+	g.RemoveEdge("edge-1234")
+	assert.Equal(t, false, g.HasEdge("edge-1234"))
+}
+
+func TestEdge(t *testing.T) {
+	g := New()
+
+	n1, _ := g.AddNode("node-1", "person")
+	n2, _ := g.AddNode("node-2", "person")
+	expected, _ := g.AddEdge("edge-1234", n1.UID, "knows", n2.UID, KV{Key: "since", Value: Value{Type: "string", Value: []byte("school")}})
+	actual, err := g.Edge("edge-1234")
+
+	assert.Nil(t, err)
+	assert.Equal(t, expected, actual)
+}
+
+func TestEdge_no_such_edge(t *testing.T) {
+	g := New()
+
+	actual, err := g.Edge("edge-missing")
+
+	assert.NotNil(t, err)
+	assert.Equal(t, Edge{}, actual)
+}
+
+func TestEdges(t *testing.T) {
+	g := New()
+
+	n1, _ := g.AddNode("node-1", "person")
+	n2, _ := g.AddNode("node-2", "person")
+
+	e1, _ := g.AddEdge("edge-1234", n1.UID, "knows", n2.UID)
+	e2, _ := g.AddEdge("edge-2345", n1.UID, "knows", n2.UID)
+	e3, _ := g.AddEdge("edge-3456", n1.UID, "knows", n2.UID)
+
+	expected := []Edge{e1, e2, e3}
+	actual := []Edge{}
+
+	iter := g.Edges()
+	for iter.Next() {
+		edge := iter.Value().(Edge)
+		actual = append(actual, edge)
+	}
+
+	assert.ElementsMatch(t, expected, actual)
+}
