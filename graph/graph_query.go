c5326745613333bc81948415783d46406cf5b749
Jenda Mudron <jenmud@gmail.com>
2020-05-07 13:01:51 +0000

    added in some code the add the edges when doing a match query

--- graph/graph_query.go
+++ graph/graph_query.go
@@ -1,6 +1,7 @@
 package graph
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/jenmud/draft/graph/parser/cypher"
@@ -24,7 +25,50 @@ func (g *Graph) Query(query string) (*Graph, error) {
 				for nodes.Next() {
 					node := nodes.Value().(Node)
 					if _, err := subg.AddNode(node.UID, node.Label, convertPropertiesToKV(node.Properties)...); err != nil {
-						log.Printf("[Query] %s", err)
+						log.Printf("[Query] %v", err)
+					}
+
+					// query the edges and add the attached in and out bound nodes.
+					// ()-->(node)
+					for _, edgeUID := range node.InEdges() {
+						edge, err := g.Edge(edgeUID)
+						if err != nil {
+							return subg, fmt.Errorf("[Query] Error populating edges: %v", err)
+						}
+
+						sourceNode, err := g.Node(edge.SourceUID)
+						if err != nil {
+							return subg, fmt.Errorf("[Query] Error fetching inbound node: %v", err)
+						}
+
+						if _, err := subg.AddNode(sourceNode.UID, sourceNode.Label, convertPropertiesToKV(sourceNode.Properties)...); err != nil {
+							log.Printf("[Query] Error inserting source node: %v", err)
+						}
+
+						if _, err := subg.AddEdge(edge.UID, sourceNode.UID, edge.Label, node.UID, convertPropertiesToKV(edge.Properties)...); err != nil {
+							log.Printf("[Query] Error inbound edge: %v", err)
+						}
+					}
+
+					// (node)-->()
+					for _, edgeUID := range node.OutEdges() {
+						edge, err := g.Edge(edgeUID)
+						if err != nil {
+							return subg, fmt.Errorf("[Query] Error populating edges: %v", err)
+						}
+
+						targetNode, err := g.Node(edge.TargetUID)
+						if err != nil {
+							return subg, fmt.Errorf("[Query] Error fetching outbound node: %v", err)
+						}
+
+						if _, err := subg.AddNode(targetNode.UID, targetNode.Label, convertPropertiesToKV(targetNode.Properties)...); err != nil {
+							log.Printf("[Query] Error inserting target node: %v", err)
+						}
+
+						if _, err := subg.AddEdge(edge.UID, node.UID, edge.Label, targetNode.UID, convertPropertiesToKV(edge.Properties)...); err != nil {
+							log.Printf("[Query] Error outbound edge: %v", err)
+						}
 					}
 				}
 			}
